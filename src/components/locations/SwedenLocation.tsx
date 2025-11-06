4d96ad85a32da32be6b97f0429c6d70dd9b811bf
Emer O Connor <emeroconnor333@gmail.com>
2025-11-06 19:02:46 +0000

    Fix: removed min and x button from sweden card

--- src/components/locations/SwedenLocation.tsx
+++ src/components/locations/SwedenLocation.tsx
@@ -11,15 +11,6 @@ export default function SwedenLocation() {
       >       
         <div className="flex justify-between p-3 bg-[var(--color-blue)]">
           <div className='text-white font-serif text-2xl'>/Sweden</div>
-          {/* window buttons */}
-          <div className="self-center">
-            <Image
-              src="/file-buttons.png"
-              alt="File buttons"
-              width={50}
-              height={50}
-            />
-          </div>
         </div>
         <div className="relative w-full" style={{ paddingTop: '80%' }}>
           <Image
