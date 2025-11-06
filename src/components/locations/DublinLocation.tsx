e334eb6f297f39e2c7a5d308ecda087f900548a3
Emer O Connor <emeroconnor333@gmail.com>
2025-11-06 19:01:36 +0000

    Feat: made location cards link to sites and removed min and x buttons

--- src/components/locations/DublinLocation.tsx
+++ src/components/locations/DublinLocation.tsx
@@ -3,31 +3,29 @@ import Image from "next/image";
 export default function DublinLocation() {
   return (
     <div className='flex flex-col'>
-      <div className="flex justify-between p-3 bg-[var(--color-blue)]">
-        <div className='text-white font-serif text-2xl'>/Dublin</div>
-        {/* window buttons */}
-        <div className="self-center">
+      <a
+        href="https://www.visittrinity.ie/venue/trinity-business-school/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className=""
+      >      
+        <div className="flex justify-between p-3 bg-[var(--color-blue)]">
+          <div className='text-white font-serif text-2xl'>/Dublin</div>
+        </div>
+        <div className="relative w-full" style={{ paddingTop: '80%' }}>
           <Image
-            src="/file-buttons.png"
-            alt="File buttons"
-            width={50}
-            height={50}
+            src="/dublin_location.png"
+            alt="Trinity Business School"
+            fill
+            className="object-cover"
+            priority
           />
         </div>
-      </div>
-      <div className="relative w-full" style={{ paddingTop: '80%' }}>
-        <Image
-          src="/dublin_location.png"
-          alt="Trinity Business School"
-          fill
-          className="object-cover"
-          priority
-        />
-      </div>
-      <div className="font-light bg-white p-3 text-lg">
-        <div className="font-bold">Trinity Business School</div>
-        <div>400 Hackers</div>
-      </div>
+        <div className="font-light bg-white p-3 text-lg">
+          <div className="font-bold">Trinity Business School</div>
+          <div>400 Hackers</div>
+        </div>
+      </a>
     </div>
   )
 }
