e890092f243a25a52b151d37762d850d7d853e16
Reuban Ramsden <reubanramsden@gmail.com>
2025-11-07 18:12:07 +0000

    Update fonts configuration

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,11 +1,4 @@
-import { Roboto, Libre_Baskerville } from 'next/font/google'
-
-export const roboto = Roboto({
-  subsets: ['latin'],
-  variable: '--font-roboto',
-  weight: ['300', '400', '700'],
-  display: 'swap'
-})
+import { Libre_Baskerville } from 'next/font/google'
 
 export const libreBaskerville = Libre_Baskerville({
   subsets: ['latin'],
@@ -14,4 +7,4 @@ export const libreBaskerville = Libre_Baskerville({
   display: 'swap'
 })
 
-export const fontVariables = `${roboto.variable} ${libreBaskerville.variable}`
+export const fontVariables = `${libreBaskerville.variable}`
