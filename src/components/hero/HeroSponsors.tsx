1cf7fb5f250e84f029a07065fef9887df7770e1f
Emer O Connor <emeroconnor333@gmail.com>
2025-11-07 01:56:41 +0000

    Feat: added logos for wellspin and project europe

--- src/components/hero/HeroSponsors.tsx
+++ src/components/hero/HeroSponsors.tsx
@@ -3,21 +3,22 @@ import Image from 'next/image'
 export default function HeroSponsors() {
   return (
     <div className='w-full'>
-      <div className='flex flex-wrap items-center justify-center sm:justify-start gap-4 sm:gap-6 md:gap-8'>
+      <div className='flex flex-wrap items-center justify-center sm:justify-start gap-4 sm:gap-6 md:gap-6'>
+           {/* OpenAi, Stripe, Lovable, Intercom, Project Europe, Paid, Wellspin */}    
         <div className='opacity-80 hover:opacity-100 transition-opacity'>
           <Image
-            src='/stripe_logo.svg'
-            alt='Stripe'
+            src='/openai_logo.png'
+            alt='OpenAI'
             width={80}
             height={24}
             className='h-5 sm:h-6 lg:h-[clamp(20px,2vw,28px)] w-auto'
           />
-        </div>
+        </div>              
         <div className='opacity-80 hover:opacity-100 transition-opacity'>
           <Image
-            src='/intercom_logo.svg'
-            alt='Intercom'
-            width={100}
+            src='/stripe_logo_no_margins.png'
+            alt='Stripe'
+            width={80}
             height={24}
             className='h-5 sm:h-6 lg:h-[clamp(20px,2vw,28px)] w-auto'
           />
@@ -28,16 +29,16 @@ export default function HeroSponsors() {
             alt='Lovable'
             width={80}
             height={24}
-            className='h-5 sm:h-6 lg:h-[clamp(20px,2vw,28px)] w-auto'
+            className='h-5 sm:h-6 w-auto'
           />
-        </div>
+        </div>        
         <div className='opacity-80 hover:opacity-100 transition-opacity'>
           <Image
-            src='/z_logo.svg'
-            alt='Z'
-            width={24}
+            src='/intercom_logo.svg'
+            alt='Intercom'
+            width={60}
             height={24}
-            className='h-5 sm:h-6 lg:h-[clamp(20px,2vw,28px)] w-auto'
+            className='h-5 sm:h-6  w-auto'
           />
         </div>
         <div className='opacity-80 hover:opacity-100 transition-opacity'>
@@ -49,6 +50,24 @@ export default function HeroSponsors() {
             className='h-5 sm:h-6 lg:h-[clamp(20px,2vw,28px)] w-auto'
           />
         </div>
+        <div className='opacity-80 hover:opacity-100 transition-opacity'>
+          <Image
+            src='/project_europe_logo.png'
+            alt='Project Europe'
+            width={60}
+            height={24}
+            className='h-5 sm:h-6 lg:h-[clamp(20px,2vw,28px)] w-auto'
+          />
+        </div>        
+        <div className='opacity-80 hover:opacity-100 transition-opacity'>
+          <Image
+            src='/wellspin_logo.svg'
+            alt='Wellspin'
+            width={60}
+            height={24}
+            className='h-5 sm:h-6 lg:h-[clamp(20px,2vw,28px)] w-auto'
+          />
+        </div>        
       </div>
     </div>
   )
